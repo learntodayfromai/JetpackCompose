4169488bf1c82566b985be7e287f8a183e9b4567
Learn Today <learntodayfromai@gmail.com>
2023-04-03 13:16:05 -0700

    added modifiers

--- app/src/main/java/com/learntodayfromai/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/learntodayfromai/jetpackcompose/MainActivity.kt
@@ -3,13 +3,18 @@ package com.learntodayfromai.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.learntodayfromai.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,18 +37,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+data class Message(val author : String, val body : String)
 @Composable
-fun MessageCard(name : String){
-    Text(text = "Hello $name!")
+fun MessageCard(msg : Message){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "example",
+            Modifier.size(40.dp).clip(CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column() {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard()
 {
-    MessageCard(name = "me")
+    MessageCard(msg = Message("hi author", "hi body"))
 }
-@Composable
+/*@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
@@ -54,4 +73,4 @@ fun DefaultPreview() {
     JetpackComposeTheme {
         Greeting("Android")
     }
-}
+}*/
