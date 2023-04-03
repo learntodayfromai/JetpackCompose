8765c1d4303504430c7e117da8370fc2e3aa1841
Learn Today <learntodayfromai@gmail.com>
2023-04-03 13:32:21 -0700

    material design and light and dark mode

--- app/src/main/java/com/learntodayfromai/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/learntodayfromai/jetpackcompose/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.learntodayfromai.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -20,19 +22,13 @@ import com.learntodayfromai.jetpackcompose.ui.theme.JetpackComposeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*setContent {
-            JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+        setContent{
+            JetpackComposeTheme() {
+                Surface(modifier = Modifier.fillMaxSize()){
+                    MessageCard(msg = Message("hi","me"))
                 }
             }
-        }*/
-        setContent{
-            Text("Hello Me!")
+
         }
     }
 }
@@ -44,33 +40,40 @@ fun MessageCard(msg : Message){
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "example",
-            Modifier.size(40.dp).clip(CircleShape)
+            Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(9.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column() {
-            Text(text = msg.author)
+            Text(text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2)
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp){
+                Text(text = msg.body,
+                    modifier=Modifier.padding(all = 4.dp),
+                    style=MaterialTheme.typography.body2)
+            }
+
         }
     }
 
 }
 
-@Preview
+@Preview(name = "light mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
 fun PreviewMessageCard()
 {
-    MessageCard(msg = Message("hi author", "hi body"))
-}
-/*@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
+    JetpackComposeTheme() {
+        Surface{
+            MessageCard(msg = Message("hi author", "hi body its me a long paragraph lets go"))
+        }
     }
-}*/
+}
